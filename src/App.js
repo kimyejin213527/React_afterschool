6a4c16084e0f92a0855857e79a8cb1cd699038b0
yejin <yejin213527@gmail.com>
2023-04-18 19:44:12 +0900

    0418_test

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  // let post = '찬실이는 복도 많지'
+
+  // document.querySelectorAll('h4')[0].innerHTML = post;
+  // let [제목1, b1] = useState('찬실이는 복도 많지');
+  // let [제목2, b2] = useState('내 서랍 속에 행복');
+  // let [제목3, b3] = useState('컴 온 컴 온');
+
+  let[제목들,b] = 
+  useState(['찬실이는 복도 많지','내 서랍 속에 행복','컴 온 컴 온']);
+  
+  return (
+    <div className="App">
+      <div className='App'>
+        <div className='black-nav'>
+          <h4 style={{color : 'white',fontSize : '22px'}}>힐링영화</h4>
+        </div>
+      </div>
+      <div className='list'>
+        <h4>{제목1}</h4>
+        <p>4월18일</p>
+      </div>
+      <div className='list'>
+        <h4>{제목2}</h4>
+        <p>4월19일</p>
+      </div>
+      <div className='list'>
+        <h4>{제목3}</h4>
+        <p>4월20일</p>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
