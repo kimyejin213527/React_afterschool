e1b9d4b5cccef07d8ad4999b62eeac6cedc5106e
yejin <yejin213527@gmail.com>
2023-04-18 19:44:23 +0900

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   let[제목들,b] = 
   useState(['찬실이는 복도 많지','내 서랍 속에 행복','컴 온 컴 온']);
-  
+
   return (
     <div className="App">
       <div className='App'>
@@ -21,15 +21,15 @@ function App() {
         </div>
       </div>
       <div className='list'>
-        <h4>{제목1}</h4>
+        <h4>{제목[0]}</h4>
         <p>4월18일</p>
       </div>
       <div className='list'>
-        <h4>{제목2}</h4>
+        <h4>{제목[1]}</h4>
         <p>4월19일</p>
       </div>
       <div className='list'>
-        <h4>{제목3}</h4>
+        <h4>{제목[2]}</h4>
         <p>4월20일</p>
       </div>
     </div>
