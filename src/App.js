2f0a01fa8191c039bede410aa53758c9c8d48a87
yejin <yejin213527@gmail.com>
2023-04-20 20:01:39 +0900

    0420

--- src/App.js
+++ src/App.js
@@ -6,33 +6,82 @@ function App() {
   //let post = '찬실은복도많지!'
   
   let [제목들, 제목변경] = useState(['찬실은복도많지','내서랍속에행복','컴온컴온']);
-  let[like, likeAdd] = useState(1)
+  let[like, likeAdd] = useState(0);
+  let[modal, setModal] = useState(false);
+
+  [1,2,3,4,5].map(function(a){
+    console.log(a)
+  })
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 style = {{color : 'white', fontSize : '22px'}}>힐링영화</h4>
+        <h4 style = {{color : 'red', fontSize : '16px'}}>힐링영화</h4>
       </div>
       <button onClick={() => {
-        let copy1 = [제목들];
+        let copy1 = [...제목들];
         copy1[0] = '벌새';
-        copy1[0] = '벌새';
-        copy1[0] = '벌새';
+        //copy1[1] = '우리들';
+        //copy1[2] = '가족';
         제목변경(copy1);
       }}>신규영화</button>
-      <div className='list'>
-        <h4>{제목들[0]}<span onClick={() => {likeAdd(like+1)}}>❤</span>{like}</h4>
+      <button onClick={() => {
+        let copy2 = [...제목들];
+        copy2.sort();
+        제목변경(copy2);
+      }}>제목정렬</button>
+      {/* <div className='list'>
+        <h4>{제목들[0]}<span onClick={() => {likeAdd(like+1)}}>👍</span>{like}</h4>
         <p>4월18일</p>
       </div>
       <div className='list'>
-        <h4>{제목들[1]}<span onClick={() => {likeAdd(like+1)}}>❤</span>{like}</h4>
+        
+        <h4 onClick={()=>{setModal(!modal)}}>{제목들[1]}</h4>
         <p>4월18일</p>
       </div>
       <div className='list'>
-        <h4>{제목들[2]}<span onClick={() => {likeAdd(like+1)}}>❤</span>{like}</h4>
+        <h4>{제목들[2]}</h4>
         <p>4월18일</p>
-      </div>
+      </div> */}
+      {/* <Modal></Modal> */}
+      {/* <Modal/> */}
+      {
+        modal == true ? <Modal/> : null
+      }
+      {/* {
+         제목들.map(function(e) {
+          return (
+            <div className='list'>
+                <h4>{e}</h4>
+                <p>4월18일</p>
+            </div>
+          )
+        })
+      } */}
+     {
+         제목들.map(function(e) {
+          return (
+            <div className='list'>
+                <h4>{e}</h4>
+                <p>4월18일</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <>
+    <div className='modal'>
+         <h4>제목</h4>
+         <p>날짜</p>
+         <p>상세내용</p>
+      </div>
+    </>
+    
+  )
 }
 
 export default App;
